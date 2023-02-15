fd52b6dd1c64acfc790b047bed3623e8be9a137d
paullaster <paullasterokoth98@gmail.com>
2023-02-15 21:43:18 +0300

    commit project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>E-Fund Raiser</div>
-  )
-}
+    <div>
+      <h1 className="heading__primary">E-Fund Raiser</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
