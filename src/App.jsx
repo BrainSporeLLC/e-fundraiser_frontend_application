324c6149a6e357a8c192331ba8ace0a3fac3f074
paullaster <paullasterokoth98@gmail.com>
2023-02-15 21:54:32 +0300

    commit project files

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
       <h1 className="heading__primary">E-Fund Raiser</h1>
       <h2 className="heading__secondary">We are your best online fundraising patners</h2>
       <h3 className="heading_tertiary">Tried and tested</h3>
+      <p>a hand that giveth is the hand that receiveth</p>
     </div>
   );
 };
