6e6d638d4c433ece4798a996ef1d02389c52799e
paullaster <paullasterokoth98@gmail.com>
2023-02-15 21:47:31 +0300

    commit project files

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ const App = () => {
   return (
     <div>
       <h1 className="heading__primary">E-Fund Raiser</h1>
+      <h2 className="heading__secondary">We are your best online fundraising patners</h2>
+      <h3 className="heading_tertiary">Tried and tested</h3>
     </div>
   );
 };
