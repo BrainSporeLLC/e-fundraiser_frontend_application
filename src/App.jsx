07ad2a67b234b7de72749d8c0f0b6ca4444bde8d
paullaster <paullasterokoth98@gmail.com>
2023-02-16 20:52:40 +0300

    commit files

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,6 @@ import { Outlet } from "react-router-dom";
 const App = () => {
   return (
     <div>
-      <h1 className="heading__primary">E-Fund Raiser</h1>
-      <h2 className="heading__secondary">We are your best online fundraising patners</h2>
-      <h3 className="heading_tertiary">Tried and tested</h3>
-      <p>a hand that giveth is the hand that receiveth</p>
-
       <section>
         <Outlet />
       </section>
