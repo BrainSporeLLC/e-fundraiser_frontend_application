198c673b5a589eccebac4adf5ecdd390b04b5900
paullaster <paullasterokoth98@gmail.com>
2023-02-20 23:26:51 +0300

    mixins add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
-        <div className="navLogo">
-        <NavLink to='/'>e-fundraiser</NavLink>
-        </div>
-        <NavLink to='raisefunds'>raise funds</NavLink>
-        <NavLink to='testimonies'>testimonies</NavLink>
-        <NavLink to='resources'>reads</NavLink>
-        <NavLink to='login'>login</NavLink>
+      <div className="navLogo">
+        <NavLink to="/">e-fundraiser</NavLink>
+      </div>
+      <div className="menus">
+        <NavLink to="raisefunds">raise funds</NavLink>
+        <NavLink to="testimonies">testimonies</NavLink>
+        <NavLink to="resources">reads</NavLink>
+        <NavLink to="login">login</NavLink>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
