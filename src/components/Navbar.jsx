b8d1a5a1c7a831898405e6f729aa724fcfb51cab
paullaster <paullasterokoth98@gmail.com>
2023-02-20 23:24:34 +0300

    mixins add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,9 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav>
+        <div className="navLogo">
         <NavLink to='/'>e-fundraiser</NavLink>
+        </div>
         <NavLink to='raisefunds'>raise funds</NavLink>
         <NavLink to='testimonies'>testimonies</NavLink>
         <NavLink to='resources'>reads</NavLink>
