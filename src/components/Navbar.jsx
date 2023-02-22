0d1d6ad68044453dd815a771f84610d16861568a
paullaster <paullasterokoth98@gmail.com>
2023-02-22 04:46:50 +0300

    add changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
 const Navbar = () => {
   return (
@@ -7,6 +8,7 @@ const Navbar = () => {
       <div className="navLogo">
         <NavLink to="/">e-fundraiser</NavLink>
       </div>
+      <Sidebar
       <div className="menus">
         <NavLink to="raisefunds">raise funds</NavLink>
         <NavLink to="testimonies">testimonies</NavLink>
