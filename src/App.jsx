6e519bea94e89caac67e1389a5216e022f9ded93
paullaster <paullasterokoth98@gmail.com>
2023-02-22 06:21:33 +0300

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const App = () => {
+  const [sidabarNavVisible, setSidebarNavVisible] = useState(false);
   return (
     <>
       <Navbar />
