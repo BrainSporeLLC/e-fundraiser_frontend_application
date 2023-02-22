a7ee9c898adcace4dd118ab837d91eab555347dc
paullaster <paullasterokoth98@gmail.com>
2023-02-23 01:00:34 +0300

    add files

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,11 +6,9 @@ const Homepage = () => {
       <h1 className="heading__primary">
         We are your best online fundraising partners
       </h1>
-      <p>
         <blockquote>
           A hand that giveth is the hand that receiveth
         </blockquote>
-      </p>
     </div>
   );
 };
