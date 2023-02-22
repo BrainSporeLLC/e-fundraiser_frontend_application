022b3d9513dcb911b1176160ae9a6158f3a23b1b
paullaster <paullasterokoth98@gmail.com>
2023-02-23 01:04:33 +0300

    add files

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,7 +6,9 @@ const Homepage = () => {
       <h1 className="heading__primary">
         We are your best online fundraising partners
       </h1>
-        <blockquote>
+        <blockquote
+        cite='https://www.bucknerkenya.org/blessed-is-the-hand-that-giveth/#:~:text=Acts%2020%3A35%20%E2%80%9CIn%20everything,to%20give%20than%20to%20receive.%E2%80%9D'
+        >
           A hand that giveth is the hand that receiveth
         </blockquote>
     </div>
