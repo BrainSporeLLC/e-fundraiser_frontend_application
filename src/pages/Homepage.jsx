a91ed57ecdbb809cf4fc09b865d447dadbff2da1
paullaster <paullasterokoth98@gmail.com>
2023-02-22 20:58:39 +0300

    add changes

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,7 +4,6 @@ const Homepage = () => {
   return (
     <div className='homepage'>
       <h1 className="heading__primary">We are your best online fundraising partners</h1>
-      <h3 className="heading_tertiary">Tried and tested</h3>
       <p>a hand that giveth is the hand that receiveth</p>
     </div>
   );
