90ffe9c09fe8ca518b1039be3813ee1600ec202b
paullaster <paullasterokoth98@gmail.com>
2023-02-22 05:05:24 +0300

    add changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,11 +3,10 @@ import { NavLink } from "react-router-dom";
 import { GoThreeBars } from "react-icons/go";
 
 const Sidebar = () => {
-  const [activateSidebar, setActiveSidebar] = useState(false);
   return (
     <>
       <div>
-        <GoThreeBars onClick={setActiveSidebar(!activateSidebar)}/>
+        <GoThreeBars/>
       </div>
     </>
   );
