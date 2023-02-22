effdc68c57d8a25377772be81f9bbed45f2ccca3
paullaster <paullasterokoth98@gmail.com>
2023-02-22 06:35:07 +0300

    add changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = ({visible, show }) => {
         </Button>
       </div>
       <div 
-      className={ visible? "show-menu": "menus" }
+      className={ visible? "show-menu menus": "menus" }
       >
         <NavLink to="raisefunds">raise funds</NavLink>
         <NavLink to="testimonies">testimonies</NavLink>
