e297eb9a3ce9d7bb3d0a3769c15203b3b1eac3dd
paullaster <paullasterokoth98@gmail.com>
2023-02-22 23:36:57 +0300

    add files

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Homepage = () => {
   return (
-    <div className='homepage'>
-      <h1 className="heading__primary">We are your best online fundraising partners</h1>
+    <div className="homepage">
+      <h1 className="heading__primary">
+        We are your best online fundraising partners
+      </h1>
       <p>a hand that giveth is the hand that receiveth</p>
     </div>
   );
