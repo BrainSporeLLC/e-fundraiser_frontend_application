e07b8c552da4c7160856d361a83c105da7d7410f
paullaster <paullasterokoth98@gmail.com>
2023-02-22 20:57:35 +0300

    add changes

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,10 +3,7 @@ import React from "react";
 const Homepage = () => {
   return (
     <div className='homepage'>
-      <h1 className="heading__primary">E-Fund Raiser</h1>
-      <h2 className="heading__secondary">
-        We are your best online fundraising patners
-      </h2>
+      <h1 className="heading__primary">We are your best online fundraising patners</h1>
       <h3 className="heading_tertiary">Tried and tested</h3>
       <p>a hand that giveth is the hand that receiveth</p>
     </div>
