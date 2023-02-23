cd8812639489e4b8077f4d7ed8275298be0e33eb
paullaster <paullasterokoth98@gmail.com>
2023-02-23 23:17:43 +0300

    adding changes

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import FormComponent from '../components/Form'
+import LabelWithInput from '../components/LabelWithInput'
 
 const Login = () => {
   return (
     <>
       <FormComponent>
-        
+        <LabelWithInput>Username</LabelWithInput>
       </FormComponent>
     </>
   )
