5be2123ac88a6a5f53d77661db5aa39601edd064
paullaster <paullasterokoth98@gmail.com>
2023-02-24 00:01:53 +0300

    adding changes

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import FormComponent from '../components/Form'
 
 const Register = () => {
   return (
     <>
-    
+    <FormComponent method="POST">
+
+    </FormComponent>
     </>
   )
 }
