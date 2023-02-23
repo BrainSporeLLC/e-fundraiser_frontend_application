bc1ee827ac00cd91d65719c5d356c5651aa35a1c
paullaster <paullasterokoth98@gmail.com>
2023-02-23 23:26:47 +0300

    adding changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Form } from "react-router-dom";
 
-const FormComponent = ({ children }) => {
+const FormComponent = ({ method, children }) => {
   return (
     <>
-      <Form>{children}</Form>
+      <Form method={method}>{children}</Form>
     </>
   );
 };
