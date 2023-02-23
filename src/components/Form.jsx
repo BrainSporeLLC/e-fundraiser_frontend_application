d7df899a5a027f98690a669f6261d568f4ca1e3d
paullaster <paullasterokoth98@gmail.com>
2023-02-23 23:09:25 +0300

    adding changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Form } from 'react-router-dom';
 
-const FormComponent = ({onSubmit, children}) => {
+const FormComponent = ({children}) => {
   return (
     <>
       <Form>
