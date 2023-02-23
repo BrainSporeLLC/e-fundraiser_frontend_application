c72f023db65e3ca47feb546eecd384f1ba3ceb12
paullaster <paullasterokoth98@gmail.com>
2023-02-23 23:13:12 +0300

    adding changes

--- src/components/LabelWithInput.jsx
+++ src/components/LabelWithInput.jsx
@@ -4,7 +4,7 @@ const LabelWithInput = ({type='text', id, name, onChange, children}) => {
   return (
     <div className='form-group'>
       <label htmlFor={id}>{children}</label>
-      <input id={id} name={name} onChange={onChange} />
+      <input type={type} id={id} name={name} onChange={onChange} />
     </div>
   )
 }
