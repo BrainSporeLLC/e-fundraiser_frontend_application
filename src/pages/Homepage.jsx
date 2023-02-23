99babda29a94f5ecdf5db74c22a60d93e7956c68
paullaster <paullasterokoth98@gmail.com>
2023-02-23 21:49:48 +0300

    adding changes

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -11,7 +11,9 @@ const Homepage = () => {
         <blockquote
         cite='https://www.bucknerkenya.org/blessed-is-the-hand-that-giveth/#:~:text=Acts%2020%3A35%20%E2%80%9CIn%20everything,to%20give%20than%20to%20receive.%E2%80%9D'
         >
-         A hand that giveth is the hand that receiveth
+          <span>&#8220;</span>
+          A hand that giveth is the hand that receiveth
+          <span>&#8221;</span>
         </blockquote>
         <cite>Bible, Act 20:35</cite>
         <NavLink className="btn center" to="signup">Signup</NavLink>
