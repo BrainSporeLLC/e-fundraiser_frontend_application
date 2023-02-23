1cf72e816cf529ce817fc231a3c79aa8a362a801
paullaster <paullasterokoth98@gmail.com>
2023-02-23 23:16:55 +0300

    adding changes

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import FormComponent from '../components/Form'
 
 const Login = () => {
   return (
     <>
-    
+      <FormComponent>
+        
+      </FormComponent>
     </>
   )
 }
