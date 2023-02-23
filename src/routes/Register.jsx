c79958594aaf737125560e1d67173915c7def7a7
paullaster <paullasterokoth98@gmail.com>
2023-02-24 00:30:20 +0300

    adding changes

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,58 +1,58 @@
-import React from 'react'
-import FormComponent from '../components/Form'
-import LabelWithInput from '../components/LabelWithInput'
-import Button from '../components/Button'
+import React from "react";
+import FormComponent from "../components/Form";
+import LabelWithInput from "../components/LabelWithInput";
+import Button from "../components/Button";
 
-const Register = ({onChange, onClick}) => {
+const Register = ({ onChange, onClick }) => {
   return (
     <>
-    <FormComponent method="POST">
-      <LabelWithInput
-      type='text'
-      id='firstName'
-      name='firstName'
-      onChange={onChange}
-      >
-        First name
-      </LabelWithInput>
-      <LabelWithInput
-      type='text'
-      id='lastName'
-      name='lastName'
-      onChange={onChange}
-      >
-        Last name
-      </LabelWithInput>
-      <LabelWithInput
-      type='email'
-      id='email'
-      name='email'
-      placeholder='e.g. someone@example.com'
-      onChange={onChange}
-      >
-        Email
-      </LabelWithInput>
-      <LabelWithInput
-      type='password'
-      id='password'
-      name='password'
-      onChange={onChange}
-      >
-        Password
-      </LabelWithInput>
+      <FormComponent method="POST">
+        <LabelWithInput
+          type="text"
+          id="firstName"
+          name="firstName"
+          onChange={onChange}
+        >
+          First name
+        </LabelWithInput>
+        <LabelWithInput
+          type="text"
+          id="lastName"
+          name="lastName"
+          onChange={onChange}
+        >
+          Last name
+        </LabelWithInput>
+        <LabelWithInput
+          type="email"
+          id="email"
+          name="email"
+          placeholder="e.g. someone@example.com"
+          onChange={onChange}
+        >
+          Email
+        </LabelWithInput>
+        <LabelWithInput
+          type="password"
+          id="password"
+          name="password"
+          onChange={onChange}
+        >
+          Password
+        </LabelWithInput>
 
-      <LabelWithInput
-      type='password'
-      id='confirmPassword'
-      name='confirmPassword' 
-      onChange={onChange}
-      >
-        Confrim password
-      </LabelWithInput>
-      <Button onClick={onClick}>Create account</Button>
-    </FormComponent>
+        <LabelWithInput
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          onChange={onChange}
+        >
+          Confrim password
+        </LabelWithInput>
+        <Button onClick={onClick}>Create account</Button>
+      </FormComponent>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
