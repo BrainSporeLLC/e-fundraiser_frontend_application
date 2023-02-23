db2b58a70890c5bdf39d0e7d4cc7aa61b4741e81
paullaster <paullasterokoth98@gmail.com>
2023-02-23 23:12:39 +0300

    adding changes

--- src/components/LabelWithInput.jsx
+++ src/components/LabelWithInput.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LabelWithInput = ({type='text', id, name, onChange, children}) => {
   return (
     <div className='form-group'>
-      <label htmlFor=""></label>
+      <label htmlFor={id}>{children}</label>
     </div>
   )
 }
