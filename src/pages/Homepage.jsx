79109712bae403578a5c528488dc0f5eba3df5bd
paullaster <paullasterokoth98@gmail.com>
2023-02-27 21:22:26 +0300

    make changes and refactor code

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,14 +6,15 @@ const Homepage = () => {
   return (
     <div className="homepage">
       <h1 className="heading__primary">
-        We are your best online fundraising partners
+        <span>We are your best online</span>
+        <span>fundraising partners</span>
       </h1>
-        <blockquote cite="https://www.bucknerkenya.org/blessed-is-the-hand-that-giveth/#:~:text=Acts%2020%3A35%20%E2%80%9CIn%20everything,to%20give%20than%20to%20receive.%E2%80%9D">
-          <span className="openning-quote">&#8220;</span>A hand that giveth is
-          the hand that receiveth
-          <span className="closing-quote">&#8221;</span>
-        </blockquote>
-        <cite>Bible, Act 20:35</cite>
+      <blockquote cite="https://www.bucknerkenya.org/blessed-is-the-hand-that-giveth/#:~:text=Acts%2020%3A35%20%E2%80%9CIn%20everything,to%20give%20than%20to%20receive.%E2%80%9D">
+        <span className="openning-quote">&#8220;</span>A hand that giveth is the
+        hand that receiveth
+        <span className="closing-quote">&#8221;</span>
+      </blockquote>
+      <cite>Bible, Act 20:35</cite>
       <NavLink className="btn center" to="donate">
         donate
       </NavLink>
