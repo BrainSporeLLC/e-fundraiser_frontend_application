3daaae4a29e2ffde42729b88a0ee3cfc56c1b845
paullaster <paullasterokoth98@gmail.com>
2023-03-02 00:17:20 +0300

    commit changes

--- src/routes/SingleProgram.jsx
+++ src/routes/SingleProgram.jsx
@@ -1,5 +1,26 @@
 import React from 'react'
 
+export const loader = async () => {
+    const dummyDanotions = [
+      {
+        name: "Middle Children",
+        desc: "lorem ipsum dolor sit amet, consectetur adip",
+        id: 1
+      },
+      {
+        name: "Middle Children",
+        desc: "lorem ipsum dolor sit amet, consectetur adip",
+        id: 2
+      },
+      {
+        name: "Middle Children",
+        desc: "lorem ipsum dolor sit amet, consectetur adip",
+        id: 3
+      }
+    ];
+    return dummyDanotions;
+  };
+
 const SingleProgram = () => {
   return (
     <div>SingleProgram</div>
