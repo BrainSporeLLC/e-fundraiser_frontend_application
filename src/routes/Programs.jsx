35d14ccbfd88b58a7b793084cbbfab968cb2b035
paullaster <paullasterokoth98@gmail.com>
2023-03-01 23:32:17 +0300

    commit changes

--- src/routes/Programs.jsx
+++ src/routes/Programs.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const  Programs = () => {
+  const loader = async() => {
+    
+  }
   return (
     <div>
       <h1 className="heading__primary">
