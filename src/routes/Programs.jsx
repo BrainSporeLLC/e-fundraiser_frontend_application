7691c70bc298a90b85fa7665e4329fa27f6fd62a
paullaster <paullasterokoth98@gmail.com>
2023-03-02 00:13:51 +0300

    commit changes

--- src/routes/Programs.jsx
+++ src/routes/Programs.jsx
@@ -1,45 +1,45 @@
-import React from 'react'
-import { NavLink, useLoaderData } from 'react-router-dom'
+import React from "react";
+import { NavLink, useLoaderData } from "react-router-dom";
 
-export  const loader = async() => {
+export const loader = async () => {
   const dummyDanotions = [
     {
-      name: 'Middle Children',
-      desc: 'lorem ipsum dolor sit amet, consectetur adip',
+      name: "Middle Children",
+      desc: "lorem ipsum dolor sit amet, consectetur adip",
       id: 1
     },
     {
-      name: 'Middle Children',
-      desc: 'lorem ipsum dolor sit amet, consectetur adip',
+      name: "Middle Children",
+      desc: "lorem ipsum dolor sit amet, consectetur adip",
       id: 2
     },
     {
-      name: 'Middle Children',
-      desc: 'lorem ipsum dolor sit amet, consectetur adip',
+      name: "Middle Children",
+      desc: "lorem ipsum dolor sit amet, consectetur adip",
       id: 3
     }
-  ]
+  ];
   return dummyDanotions;
-}
+};
 
-const  Programs = () => {
+const Programs = () => {
   const programs = useLoaderData();
-  console.log(programs)
+  console.log(programs);
   return (
     <div>
-      {
-        programs.map(({name, desc, id}) =>{
-          return (
+      {programs.map(({ name, desc, id }) => {
+        return (
+          <NavLink to={`programs/${id}`}>
             <div key={id}>
               <h3>{name}</h3>
               <p>{desc}</p>
               <NavLink to="/donate">Donate</NavLink>
             </div>
-          )
-        })
-      }
+          </NavLink>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Programs
+export default Programs;
