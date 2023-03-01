1e16935ca22f8bc1e0b26b1ea3123effe20724ce
paullaster <paullasterokoth98@gmail.com>
2023-03-02 00:20:58 +0300

    commit changes

--- src/routes/SingleProgram.jsx
+++ src/routes/SingleProgram.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useLoaderData } from 'react-router-dom';
+import { NavLink, useLoaderData } from 'react-router-dom';
 
 export const loader = async () => {
     const dummyDanotions = [
