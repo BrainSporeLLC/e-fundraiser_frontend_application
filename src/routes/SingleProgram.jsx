be07cf59bf1f746985c91f7fab92400de58a7d47
paullaster <paullasterokoth98@gmail.com>
2023-03-02 00:32:57 +0300

    commit changes

--- src/routes/SingleProgram.jsx
+++ src/routes/SingleProgram.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import { NavLink, useLoaderData } from 'react-router-dom';
 
-export const loader = async (params) => {
-    
-  };
-
 const SingleProgram = () => {
-  const programs = useLoaderData();
-  console.log("programs");
   return (
     <div>
       Single program appears here
