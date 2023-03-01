71cc355cd81c789593d26425c3fea6f7fa2f9450
paullaster <paullasterokoth98@gmail.com>
2023-03-02 00:32:40 +0300

    commit changes

--- src/routes/SingleProgram.jsx
+++ src/routes/SingleProgram.jsx
@@ -1,26 +1,8 @@
 import React from 'react'
 import { NavLink, useLoaderData } from 'react-router-dom';
 
-export const loader = async (param) => {
-    return param.programId;
-    // const dummyDanotions = [
-    //   {
-    //     name: "Middle Children",
-    //     desc: "lorem ipsum dolor sit amet, consectetur adip",
-    //     id: 1
-    //   },
-    //   {
-    //     name: "Middle Children",
-    //     desc: "lorem ipsum dolor sit amet, consectetur adip",
-    //     id: 2
-    //   },
-    //   {
-    //     name: "Middle Children",
-    //     desc: "lorem ipsum dolor sit amet, consectetur adip",
-    //     id: 3
-    //   }
-    // ];
-    // return dummyDanotions.find(dummy => dummy.id === id);
+export const loader = async (params) => {
+    
   };
 
 const SingleProgram = () => {
