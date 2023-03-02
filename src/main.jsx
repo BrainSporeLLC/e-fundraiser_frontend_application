6fc6f387047997e476b28050fc7635f0a4143b94
paullaster <paullasterokoth98@gmail.com>
2023-03-02 22:46:58 +0300

    commit files

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import Programs, {
   loader as programsLoader
 } from './routes/Programs'
 import SingleProgram, {
-  // loader as singleProgramLoader
+loader as singleProgramLoader
 } from './routes/SingleProgram'
 import Reads from './routes/Reads'
 import Homepage from './pages/Homepage'
