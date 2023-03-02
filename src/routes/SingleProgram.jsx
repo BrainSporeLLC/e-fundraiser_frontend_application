e37c0e55c2998de4c3d5de49f3f42832bc87f037
paullaster <paullasterokoth98@gmail.com>
2023-03-02 22:46:00 +0300

    commit files

--- src/routes/SingleProgram.jsx
+++ src/routes/SingleProgram.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { NavLink, useLoaderData } from 'react-router-dom'
 
 export const loader = async(params) => {
-  const programId = params.programId
+  const programId = params.programId;
+  return programId;
 }
 
 const SingleProgram = () => {
