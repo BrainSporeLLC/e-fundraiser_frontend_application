f2fbf5393263e1a9f1fa52cdb38e97162df4b0f8
paullaster <paullasterokoth98@gmail.com>
2023-03-02 22:47:07 +0300

    commit files

--- src/main.jsx
+++ src/main.jsx
@@ -37,7 +37,7 @@ const app = createBrowserRouter(
       />
       <Route
       element={<SingleProgram/>}
-      // loader={singleProgramLoader}
+      loader={singleProgramLoader}
       path='programs/:programId'
       />
       <Route
